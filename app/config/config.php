6de157f830a6298559d5769b8ffd14f79f759e25
wegizul <mrwg406@gmail.com>
2023-06-26 16:39:01 +0700

    ubah warna template

--- app/config/config.php
+++ app/config/config.php
@@ -18,7 +18,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/posbabada/';
+$config['base_url'] = 'http://localhost/posbabada-new/';
 
 /*
 |--------------------------------------------------------------------------
