365b8a76f40eb2f5099ca3244aeb723f1629cceb
babadanunu <ibnu.muhammad27@gmail.com>
2023-06-28 16:29:56 +0700

    struk

--- themes/default/admin/views/pos/view.php
+++ themes/default/admin/views/pos/view.php
@@ -201,17 +201,18 @@
                                     foreach ($rows as $row) {
                                         if ($pos_settings->item_order == 1 && $category != $row->category_id) {
                                             $category = $row->category_id;
-                                            echo '<tr><td colspan="100%" class="no-border"><strong>' . $row->category_name . '</strong></td></tr>';
+                                            echo '<tr><td colspan="2" class="no-border border-bottom"><strong>' . $row->category_name . '</strong></td></tr>';
                                         }
-                                        echo '<tr><td colspan="2" class="no-border">#' . $r . ': &nbsp;&nbsp;' . product_name($row->product_name, ($printer ? $printer->char_per_line : null)) . ($row->variant ? ' (' . $row->variant . ')' : ''), ($row->serial_no ? '<br>' . $row->serial_no : '') . '<span class="pull-right">' . ($row->tax_code ? '*' . $row->tax_code : '') . '</span></td></tr>';
+                                        echo '<tr><td colspan="2" class="no-border border-bottom">#' . $r . ': &nbsp;&nbsp;' . product_name($row->product_name, ($printer ? $printer->char_per_line : null)) . '<span style="text-align:center"> ' . $this->sma->formatQuantity($row->unit_quantity) . ($row->product_unit_quantity ? $row->unit_quantity : '') . ' x ' . ($row->item_discount != 0 ? '(' . $this->sma->formatMoney($row->unit_price + ($row->item_discount / $row->unit_quantity)) . ' - ' . $this->sma->formatMoney($row->item_discount / $row->unit_quantity) . ')' : $this->sma->formatMoney($row->unit_price)) . '</span><span class="pull-right">' .  $this->sma->formatMoney($row->subtotal) . '</span></td></tr>';
+                                        echo '<tr><td class="no-border">' . ($row->item_tax != 0 ? ' [' . lang('tax') . ' <small>(' . ($Settings->indian_gst ? $row->tax : $row->tax_code) . ')</small> ' . $this->sma->formatMoney($row->item_tax) . ($row->hsn_code ? ' (' . lang($row->product_type == 'service' ? 'sac_code' : 'hsn_code') . ': ' . $row->hsn_code . ')' : '') . ']' : '') . '</td><td colspan="4" class= "border-bottom">' . '</td></tr>';
+
                                         if (!empty($row->second_name)) {
-                                            echo '<tr><td colspan="2" class="no-border">' . $row->second_name . '</td></tr>';
+                                            echo '<tr><td colspan="4" class="no-border">' . $row->second_name . '</td></tr>';
                                         }
                                         if (!empty($row->comment)) {
-                                            echo '<tr><td colspan="2" class="no-border">' . $row->comment . '</td></tr>';
+                                            echo '<tr><td colspan="4" class="no-border">' . $row->comment . '</td></tr>';
                                         }
-                                        echo '<tr><td class="no-border border-bottom">' . $this->sma->formatQuantity($row->unit_quantity) . ($row->product_unit_code ? $row->product_unit_code : '') . ' x ' . ($row->item_discount != 0 ? '(' . $this->sma->formatMoney($row->unit_price + ($row->item_discount / $row->unit_quantity)) . ' - ' . $this->sma->formatMoney($row->item_discount / $row->unit_quantity) . ')' : $this->sma->formatMoney($row->unit_price)) . ($row->item_tax != 0 ? ' [' . lang('tax') . ' <small>(' . ($Settings->indian_gst ? $row->tax : $row->tax_code) . ')</small> ' . $this->sma->formatMoney($row->item_tax) . ($row->hsn_code ? ' (' . lang($row->product_type == 'service' ? 'sac_code' : 'hsn_code') . ': ' . $row->hsn_code . ')' : '') . ']' : '') . '</td><td class="no-border border-bottom text-right">' . $this->sma->formatMoney($row->subtotal) . '</td></tr>';
-
+                                        
                                         $r++;
                                     }
                                     if ($return_rows) {
@@ -221,8 +222,8 @@
                                                 $category = $row->category_id;
                                                 echo '<tr><td colspan="100%" class="no-border"><strong>' . $row->category_name . '</strong></td></tr>';
                                             }
-                                            echo '<tr><td colspan="2" class="no-border">#' . $r . ': &nbsp;&nbsp;' . product_name($row->product_name, ($printer ? $printer->char_per_line : null)) . ($row->variant ? ' (' . $row->variant . ')' : '') . ($row->serial_no ? '<br>' . $row->serial_no : '') . '<span class="pull-right">' . ($row->tax_code ? '*' . $row->tax_code : '') . '</span></td></tr>';
-                                            echo '<tr><td class="no-border border-bottom">' . $this->sma->formatQuantity($row->unit_quantity) . ($row->product_unit_code ? $row->product_unit_code : '') . ' x ' . $this->sma->formatMoney($row->unit_price) . ($row->item_tax != 0 ? ' - ' . lang('tax') . ' <small>(' . ($Settings->indian_gst ? $row->tax : $row->tax_code) . ')</small> ' . $this->sma->formatMoney($row->item_tax) . ($row->hsn_code ? ' (' . lang($row->product_type == 'service' ? 'sac_code' : 'hsn_code') . ': ' . $row->hsn_code . ')' : '') : '') . '</td><td class="no-border border-bottom text-right">' . $this->sma->formatMoney($row->subtotal) . '</td></tr>';
+                                            echo '<tr><td colspan="4" class="no-border">#' . $r . ': &nbsp;&nbsp;' . product_name($row->product_name, ($printer ? $printer->char_per_line : null)) . ($row->variant ? ' (' . $row->variant . ')' : '') . ($row->serial_no ? '<br>' . $row->serial_no : '') . '<span class="pull-right">' . ($row->tax_code ? '*' . $row->tax_code : '') . '</span></td></tr>';
+                                            echo '<tr><td class="no-border border-bottom">' . $this->sma->formatQuantity($row->unit_quantity) . ($row->product_unit_code ? $row->product_unit_code : '') . ' x ' . $this->sma->formatMoney($row->unit_price) . ($row->item_tax != 0 ? ' - ' . lang('tax') . ' <small>(' . ($Settings->indian_gst ? $row->tax : $row->tax_code) . ')</small> ' . $this->sma->formatMoney($row->item_tax) . ($row->hsn_code ? ' (' . lang($row->product_type == 'service' ? 'sac_code' : 'hsn_code') . ': ' . $row->hsn_code . ')' : '') : '') . '</td></tr>';
 
                                             // echo '<tr><td class="no-border border-bottom">' . $this->sma->formatQuantity($row->quantity) . ' x ';
                                             // if ($row->item_discount != 0) {
@@ -283,6 +284,8 @@
                                             <th><?= lang('grand_total'); ?></th>
                                             <th class="text-right"><?= $this->sma->formatMoney($return_sale ? (($inv->grand_total + $inv->rounding) + $return_sale->grand_total) : ($inv->grand_total + $inv->rounding)); ?></th>
                                         </tr>
+                                        <tr>
+                                            <td> <?= ($row->tax_code ? '*' . $row->tax_code : '') ?> </td></tr>
                                     <?php
                                     } else {
                                     ?>
@@ -368,6 +371,7 @@
                                         echo $payment->note ? '</tr><td colspan="4">' . lang('payment_note') . ': ' . $payment->note . '</td>' : '';
                                     }
                                     echo '</tr>';
+                                    
                                 }
                                 echo '</tbody></table>';
                             }
