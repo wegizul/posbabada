93c5ae68d0a19bdb3666081eadfae67a07bba930
wegizul <mrwg406@gmail.com>
2024-04-16 15:58:14 +0700

    api logistik

---
+++ app/controllers/Api.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Api extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        date_default_timezone_set('Asia/Jakarta');
+    }
+
+    public function getWarehouses()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $data = $this->site->getAllWarehouses();
+
+            $json_data = json_encode($data);
+
+            header('Content-Type: application/json');
+            echo $json_data;
+        } else {
+            http_response_code(405);
+            echo json_encode(array('message' => 'Method Not Allowed'));
+        }
+    }
+}
